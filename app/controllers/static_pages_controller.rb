6d0e87250ae061cb18b0ac3db737545807a214c7
Peter Abah <peterabah.ob@gmail.com>
2021-07-31 21:27:42 +0100

    add view for index action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,25 @@
 class StaticPagesController < ApplicationController
+  def index
+    @photos_url = params[:query] ? get_photos(params[:query]) : random_photos
+  end
+
+  private
+
+  def random_photos
+    begin
+      Unsplash::Photo.random(count: 10).map { |photo| photo[:urls][:small] }
+    rescue Unsplash::Error
+      []
+    end
+  end
+
+  def get_photos(query)
+    begin
+      result = Unsplash::Photo.search(query)
+      result.map { |photo| photo[:urls][:small] }
+    rescue Unsplash::Error => e
+      puts e
+      []
+    end
+  end
 end
